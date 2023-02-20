3b29f2beaa2e9880fe01fa433608cc15abf70ad0
malkochilal <119036086+malkochilal@users.noreply.github.com>
2023-02-21 01:37:56 +0300

    Main

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const server = require('./api/server');
 
-const port = 9000;
+const port = 9002;
+
+server.listen(port, () => {
+    console.log("Server is working at" + port);
+});
 
 // START YOUR SERVER HERE
